624dfb3bb06b6c0f7723afa79956a6c49ab07069
Sohan2026 <sraval@ucsd.edu>
2025-01-31 22:58:03 -0800

    commit

--- projects/projects.js
+++ projects/projects.js
@@ -1,20 +1,18 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
-// Fetch project data from JSON
 async function loadProjects() {
     try {
         const projects = await fetchJSON('../lib/projects.json');
 
-        // Get the projects container element
+
         const projectsContainer = document.querySelector('.projects');
 
-        // Get the projects-title element and update it with the count
+
         const projectsTitle = document.querySelector('.projects-title');
         if (projectsTitle) {
-            projectsTitle.textContent = `Projects (${projects.length})`; // Update the count
+            projectsTitle.textContent = `Projects (${projects.length})`;
         }
 
-        // If projects data is empty, display a placeholder message
         if (projects && projects.length > 0) {
             projects.forEach(project => {
                 renderProjects(project, projectsContainer, 'h2');
@@ -29,5 +27,4 @@ async function loadProjects() {
     }
 }
 
-// Call the function to load projects when the page loads
 loadProjects();
